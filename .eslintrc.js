d65b16ba0af06e2447868e4eec6fc6e75e991887
siddhiganesh <siddhiganesh39@gmail.com>
2025-09-29 20:49:52 +0530

    second commit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:nuxt/recommended",
+    "standard",
+  ],
+  rules: {
+    // your custom rules
+  },
+};
