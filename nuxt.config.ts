5694e8cb3a16f18685349f7ed40bdf85e0443c3d
siddhiganesh <siddhiganesh39@gmail.com>
2025-09-30 00:41:06 +0530

    icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/icon"],
-  css: ["../assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
