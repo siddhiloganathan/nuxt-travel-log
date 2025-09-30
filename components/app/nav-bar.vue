a5d22f064d4f5a8f50150e323f4f80b661096f8c
siddhiganesh <siddhiganesh39@gmail.com>
2025-09-30 10:25:58 +0530

    added landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign in with Github
+        <Icon name="tabler:brand-github" size="24" /></a>
+    </div>
+  </div>
+</template>
