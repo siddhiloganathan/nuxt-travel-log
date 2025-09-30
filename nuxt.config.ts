51c9dce2987c1c1f936d517463044f0e1a3f83b5
siddhiganesh <siddhiganesh39@gmail.com>
2025-09-30 12:05:45 +0530

    added theme toggle

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode"],
   css: ["../assets/css/main.css"],
   eslint: {
     config: {
@@ -13,4 +13,7 @@ export default defineNuxtConfig({
     },
   },
   vite: { plugins: [tailwindcss()] },
+  colorMode: {
+    dataValue: "theme",
+  },
 });
