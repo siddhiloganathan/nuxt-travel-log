5b8314841d37551bd981a60d4dcbf111e6525009
siddhiganesh <siddhiganesh39@gmail.com>
2025-10-01 19:02:25 +0530

    ssr

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,14 @@
+<script lang="ts" setup>
+// import { onMounted } from "vue";
+
+import { useAuthStore } from "../stores/auth";
+
+const authStore = useAuthStore();
+// onMounted(async () => {
+await authStore.init();
+// });
+</script>
+
 <template>
   <div class="flex flex-col min-h-screen">
     <app-nav-bar />
