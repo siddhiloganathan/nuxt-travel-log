180e00589411e775f52856e3938c8d099e1891b1
siddhiloganathan <siddhiloganathan@gmail.com>
2025-10-02 20:21:07 +0530

    data storage (#64)
    Co-authored-by: siddhiganesh <siddhiganesh39@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ import "./lib/env";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt"],
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt", "nuxt-csurf"],
   css: ["../assets/css/main.css"],
   eslint: {
     config: {
