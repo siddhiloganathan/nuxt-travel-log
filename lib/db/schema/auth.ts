631f94396ded06676d339799c580276707d05880
siddhiganesh <siddhiganesh39@gmail.com>
2025-10-03 16:35:04 +0530

    location slugs

--- lib/db/schema/auth.ts
+++ lib/db/schema/auth.ts
@@ -19,7 +19,7 @@ export const session = sqliteTable("session", {
   updatedAt: integer().notNull(),
   ipAddress: text(),
   userAgent: text(),
-  userId: text()
+  userId: int()
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
 });
@@ -28,7 +28,7 @@ export const account = sqliteTable("account", {
   id: int().primaryKey({ autoIncrement: true }),
   accountId: text().notNull(),
   providerId: text().notNull(),
-  userId: text()
+  userId: int()
     .notNull()
     .references(() => user.id, { onDelete: "cascade" }),
   accessToken: text(),
--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,11 @@
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slugify";
+
 import db from "@/lib/db";
 import { InsertLocation, location } from "@/lib/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -26,25 +32,52 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
+  const existingLocation = await db.query.location.findFirst({
+    where:
+    and(
+      eq(location.name, result.data.name),
+      eq(location.userId, event.context.user.id),
+    ),
+  });
+
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "A location with that name already exists",
+    }));
+  }
+
+  let slug = slugify(result.data.name);
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+  while (existing) {
+    const id = nanoid();
+    const idSlug = `${slug}-${id}`;
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, idSlug),
+    }));
+    if (!existing) {
+      slug = idSlug;
+    }
+  }
+
   try {
     const [created] = await db.insert(location).values({
       ...result.data,
-      slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
+      slug,
       userId: event.context.user.id,
     }).returning();
     return created;
   }
-  catch (error) {
-    console.error(error);
+  catch (error: any) {
+    if (error.cause?.message?.includes("UNIQUE constraint failed: location.slug")) {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "Slug must be unique (the location name is used to generate the slug)",
+      }));
+    }
+
     throw error;
-  //   if (e instanceof LibsqlError) {
-  //     if (e.code === "SQLITE_CONSTRAINT" && e.message.includes("location.slug")) {
-  //       return sendError(event, createError({
-  //         statusCode: 409,
-  //         statusMessage: "Slug must be unique (the location name is used to generate the slug).",
-  //       }));
-  //     }
-  //   }
-  //   throw e;
   }
 });
