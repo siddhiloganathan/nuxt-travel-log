0528785af252838207aa0258b3e193b48e8a70db
siddhiganesh <siddhiganesh39@gmail.com>
2025-10-06 02:13:09 +0530

    add map

---
+++ components/app/map.client.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { CENTER_INDIA } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() => colorMode.value === "dark" ? "/styles/dark.json" : "https://tiles.openfreemap.org/styles/liberty");
+const zoom = 3.5;
+</script>
+
+<template>
+  <MglMap :map-style="style" :center="CENTER_INDIA" :zoom="zoom">
+    <MglNavigationControl />
+  </MglMap>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,14 +7,33 @@ import "./lib/env";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxtjs/color-mode", "@pinia/nuxt", "@vee-validate/nuxt", "nuxt-csurf"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+    "@vee-validate/nuxt",
+    "nuxt-csurf",
+    "nuxt-maplibre",
+  ],
   css: ["../assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
   },
-  vite: { plugins: [tailwindcss()] },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+
+    optimizeDeps: {
+      include: [
+        "maplibre-gl",
+      ],
+    },
+  },
+
   colorMode: {
     dataValue: "theme",
   },
